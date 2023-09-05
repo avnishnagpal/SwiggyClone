169c70fb3c541123dec6b2bf2ed4fd372a8e016b
avnishnagpal <nagpalavnish11@gmail.com>
2023-09-05 23:56:04 +0530

    Practice Class Based Components in React

--- src/Routes/AboutUs.js
+++ src/Routes/AboutUs.js
@@ -1,23 +1,39 @@
 import React from "react";
 import AboutUsCardClass from "./AboutUsCardClass";
-import AboutUsCard from "./AboutUsCard";
+//import AboutUsCard from "./AboutUsCard";
 
-const AboutUs = () => {
-  return (
-    <div>
-      <h1>AboutUs</h1>
-      <AboutUsCard
-        name={"Avnish function"}
-        contact={"@nagpalavnish11"}
-        location={"Jalandhar"}
-      ></AboutUsCard>
-      <AboutUsCardClass
-        name={"Avnish class"}
-        contact={"@nagpalavnish11"}
-        location={"Jalandhar"}
-      ></AboutUsCardClass>
-    </div>
-  );
-};
+class AboutUs extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userInfo: {
+        name: "Dummy Name",
+        location: "US",
+      },
+    };
+  }
+
+  async componentDidMount() {
+    const data = await fetch("https://api.github.com/users/mojombo");
+    const json = await data.json();
+    // console.log(json);
+    this.setState({
+      userInfo: json,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>AboutUs</h1>
+        <AboutUsCardClass
+          name={this.state.userInfo.name}
+          contact={"@nagpalavnish11"}
+          location={this.state.userInfo.location}
+        ></AboutUsCardClass>
+      </div>
+    );
+  }
+}
 
 export default AboutUs;
